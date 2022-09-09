7c3cd66329f14cd73d714201f8d74e5c25be9ea3
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-09-09 10:16:39 -0700

    update page

---
+++ static/js/anim.js
@@ -0,0 +1,44 @@
+var swizzleDiv0 = $( ".anim" ).each(
+  function() {
+    $(this).attr("src", $(this).attr("png"))
+  }
+);
+
+var swizzleDivs = $( ".anim" ).hover(
+  function() {
+    $(this).attr("src", $(this).attr("png").replace(".png", ".gif"))
+  },
+  function() {
+    $(this).attr("src", $(this).attr("png"))
+  }
+);
+
+/* OLD
+
+function swizzle(name){
+    $(function() {
+        var divName  = "#" + name;
+        var animName = "/liquidhaskell-blog/static/img/" + name + ".gif";
+        var statName = "/liquidhaskell-blog/static/img/" + name + ".png";
+        $(divName).hover(
+            function() {
+                $(this).attr("src", animName);
+            },
+            function() {
+                $(this).attr("src", statName);
+            }
+        );
+    });
+};
+
+var z0 = swizzle("splash-head");
+var z1 = swizzle("splash-unstutter");
+var z2 = swizzle("splash-vectorsum");
+var z3 = swizzle("splash-dotproduct");
+var z4 = swizzle("splash-fib");
+var z5 = swizzle("splash-merge");
+var z6 = swizzle("splash-ups");
+var z7 = swizzle("splash-insertsort");
+var z8 = swizzle("splash-assocthm");
+var z9 = swizzle("splash-assocpf");
+*/
